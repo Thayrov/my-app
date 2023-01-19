09bccbe7fab156aba24073d4c6b7cf4dd9137797
thayrov <thayrovg@gmail.com>
2023-01-18 19:50:24 -0600

    modulariza estilos y cambia archivo raiz para ejecución del proyecto

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import {
 	Button,
 	FlatList,
 	Modal,
-	StyleSheet,
 	Text,
 	TextInput,
 	TouchableOpacity,
@@ -11,8 +10,9 @@ import {
 import React, {useState} from 'react';
 
 import {StatusBar} from 'expo-status-bar';
+import {styles} from './styles'
 
-export default function App() {
+const App = () => {
 	const [task, setTask] = useState('');
 	const [tasks, setTasks] = useState([]);
 	const [isModalVisible, setIsModalVisible] = useState(false);
@@ -92,80 +92,6 @@ export default function App() {
 		</View>
 	);
 }
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#424C5E',
-		height: 45,
-	},
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		paddingTop: 60,
-		paddingHorizontal: 20,
-	},
-	input: {
-		width: '88%',
-		backgroundColor: '#f9f9f9',
-		borderWidth: 1,
-		borderColor: '#ccc',
-		borderRadius: 5,
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingTop: 5,
-		paddingBottom: 5,
-		fontSize: 20,
-		color: '#424C5E',
-		fontFamily: 'monospace',
-		fontWeight: 'bold',
-	},
-	listContainer: {
-		marginHorizontal: 20,
-		marginTop: 40,
-	},
-	itemList: {
-		fontSize: 16,
-		fontWeight: 'bold',
-		color: '#424C5E',
-	},
-	itemContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		padding: 10,
-		backgroundColor: '#CDFDFA',
-		marginBottom: 10,
-		borderRadius: 5,
-	},
-	modalContainer: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		marginTop: 50,
-		paddingVertical: 20,
-	},
-	modalTitle: {
-		fontSize: 18,
-		fontWeight: 'bold',
-		marginBottom: 10,
-	},
-	modalContentContainer: {
-		paddingVertical: 20,
-	},
-	modalContent: {
-		fontSize: 14,
-		color: 'gray',
-	},
-	contentDetails: {
-		fontSize: 14,
-		color: 'black',
-		textAlign: 'center',
-		fontWeight: 'bold',
-		paddingVertical: 10,
-		marginBottom: 20,
-	},
-	modalButtonCaontainer: {
-		width: '60%',
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-	},
-});
+
+
+export default App;
